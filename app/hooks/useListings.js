ca70ea8150d88fae24d4ed076e36876e44fd6f66
rattlingmars8 <rattlingmars8@gmail.com>
2025-02-23 17:06:01 +0100

    added useListings.js like custom hook

---
+++ app/hooks/useListings.js
@@ -0,0 +1,30 @@
+import useSWR from "swr";
+
+
+export const fetcher = async (url) => {
+  const res = await fetch(url);
+  if (!res.ok) {
+    const errorInfo = await res.json();
+    const error = new Error("An error occurred while fetching the data");
+    error.info = errorInfo;
+    error.status = res.status;
+    throw error;
+  }
+  return res.json();
+};
+
+export function useListings(queryString) {
+  const {data: responseData, error: listingsError} = useSWR(
+    `/api/listings${queryString ? `?${queryString}` : ""}`,
+    fetcher
+  );
+
+  const listings = responseData?.listings || [];
+  const totalMatches = responseData?.totalMatches || 0;
+  const totalPages = responseData?.totalPages || 0;
+  const currentPage = responseData?.currentPage || 1;
+
+  return {listings, totalMatches, totalPages, currentPage, listingsError}
+
+}
+

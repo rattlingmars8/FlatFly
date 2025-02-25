714b4daeb7342f1e62d67414f050cd0a17b076d5
rattlingmars8 <rattlingmars8@gmail.com>
2025-02-25 21:00:03 +0100

    Added and connect new fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,21 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 export default {
-    content: [
-        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
-    theme: {
-        extend: {
-            colors: {
-                primary: "#7065F0",
-                purpleShades: "#E0DEF7",
-                background: "#F8F9FA",
-                text: "#000929",
-                borderGray: "#E0E5EC",
-                secondary: "#554ccf",
-            },
-        },
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#7065F0",
+        purpleShades: "#E0DEF7",
+        background: "#F8F9FA",
+        text: "#000929",
+        borderGray: "#E0E5EC",
+        secondary: "#554ccf",
+      },
+      fontFamily: {
+        quicksand: ["Quicksand", ...fontFamily.sans],
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
